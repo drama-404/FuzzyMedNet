5c5b471d8d8371c2e84eca463fd6bf44197336ea
Denada <dr7230o@gre.ac.uk>
2023-10-14 19:20:59 +0100

    implemented hybrid impute

--- src/data_imputation/hybrid_impute.py
+++ src/data_imputation/hybrid_impute.py
@@ -1,8 +1,27 @@
 import numpy as np
 import pandas as pd
+import pickle
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from config import *
+
+
+# Define a function to save a DataFrame to a pickle file
+def save_to_pickle(df, filename):
+    with open(filename, 'wb') as f:
+        pickle.dump(df, f)
+
+
+# Define a function to load a DataFrame from a pickle file
+def load_from_pickle(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+
+
+# Flags to control whether to load intermediate results from disk
+LOAD_TIME_IMPUTED = False
+LOAD_FILLED = False
+LOAD_MULTIPLE_IMPUTED = False
 
 def time_series_imputer(df):
     # Forward fill to propagate the last valid observation forward
@@ -22,15 +41,34 @@ def multiple_imputer(df):
 
 
 def hybrid_imputer(df, train_subj):
-    # Step 1: Time-series imputation
-    df_time_imputed = time_series_imputer(df)
+    # Initialize filenames
+    time_imputed_filename = 'time_imputed.pkl'
+    filled_filename = 'filled.pkl'
+    multiple_imputed_filename = 'multiple_imputed.pkl'
 
-    # Step 2: Compute mean for training subjects and fill remaining NaNs
-    train_mean = df_time_imputed.loc[train_subj].mean()
-    df_filled = df_time_imputed.fillna(train_mean)
+    print(f"Initial shape: {df.shape}")
+    if LOAD_TIME_IMPUTED and os.path.exists(time_imputed_filename):
+        df_time_imputed = load_from_pickle(time_imputed_filename)
+    else:
+        df_time_imputed = time_series_imputer(df)
+        save_to_pickle(df_time_imputed, time_imputed_filename)
+    print(f"Shape after time-series imputation: {df_time_imputed.shape}")
 
-    # Step 3: Multiple imputation for any remaining missing values
-    df_multiple_imputed = multiple_imputer(df_filled)
+    if LOAD_FILLED and os.path.exists(filled_filename):
+        df_filled = load_from_pickle(filled_filename)
+    else:
+        train_mean = df_time_imputed.loc[train_subj].mean()
+        df_filled = df_time_imputed.fillna(train_mean)
+        save_to_pickle(df_filled, filled_filename)
+    print(f"Shape after filling with mean: {df_filled.shape}")
+
+    if LOAD_MULTIPLE_IMPUTED and os.path.exists(multiple_imputed_filename):
+        df_multiple_imputed = load_from_pickle(multiple_imputed_filename)
+    else:
+        df_multiple_imputed = multiple_imputer(df_filled)
+        save_to_pickle(df_multiple_imputed, multiple_imputed_filename)
+    print(f"Shape after multiple imputation: {df_multiple_imputed.shape}")
 
     return df_multiple_imputed
+
 
