0e1608d5e54e5760b18bbc6021c4dc17414a4e27
Denada <denadaramatech@gmail.com>
2023-10-19 19:45:03 +0100

    feature engineering - vitals_labs

--- utils/eda_functions.py
+++ utils/eda_functions.py
@@ -3,7 +3,23 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import pickle
 import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+
+# Define a function to save a DataFrame to a pickle file
+def save_to_pickle(df, filename):
+    with open(filename, 'wb') as f:
+        pickle.dump(df, f)
+
+
+# Define a function to load a DataFrame from a pickle file
+def load_from_pickle(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
 
 
 def plot_categorical_columns(df, categorical_columns):
@@ -139,3 +155,73 @@ def plot_hourly_counts(df, features):
             plt.xlabel('Hours in ICU')
             plt.ylabel(f'{feature} (Sum)')
             plt.show()
+
+
+def pearson_correlation_among_features(df, fig_size=(20, 15)):
+    corr = df.corr()  # Compute the correlation matrix
+    mask = np.triu(np.ones_like(corr, dtype=bool))  # Create a mask for the upper triangle
+    annot = np.around(corr, 2)  # Format the correlation matrix for 2 decimal places
+
+    fig, ax = plt.subplots(figsize=fig_size)
+    sns.heatmap(corr, mask=mask, cmap='coolwarm', vmax=1, vmin=-1, center=0,
+                square=True, linewidths=.5, cbar_kws={"shrink": .75}, annot=annot)
+    plt.title(' Correlation Among Features')
+    plt.show()
+
+
+def pearson_highly_correlated_pairs(df, threshold=.7):
+    # Extracting pairs without duplicates
+    pairs_to_drop = set()
+    corr = df.corr()
+    cols = corr.columns
+    for i in range(0, corr.shape[1]):
+        for j in range(0, i + 1):
+            pairs_to_drop.add((cols[i], cols[j]))
+
+    # Extract the values and pairs into a DataFrame
+    corr_values = []
+    for i in range(corr.shape[0]):
+        for j in range(corr.shape[1]):
+            if np.abs(corr.iloc[i, j]) >= threshold and (cols[i], cols[j]) not in pairs_to_drop:
+                corr_values.append([cols[i], cols[j], corr.iloc[i, j]])
+
+    corr_df = pd.DataFrame(corr_values, columns=['var1', 'var2', 'corr_value'])
+    return corr_df
+
+
+def pearson_target_correlation(df, Ys):
+    X_y = pd.merge(df, Ys['mort_hosp'], left_index=True, right_index=True, how='left')
+    corr_matrix = X_y.corr()
+
+    fig, ax = plt.subplots(figsize=(5, 8))
+    # Isolate the column corresponding to `in-hospital mortality`
+    corr_target = corr_matrix[['mort_hosp']].drop(labels=['mort_hosp'])
+
+    sns.heatmap(corr_target, annot=True, fmt='.3', cmap='RdBu_r', vmax=1, vmin=-1)
+    plt.title('Pearson Target Correlation')
+    plt.show()
+
+
+def encoding_categorical_vars(df, categorical_cols):
+
+    # Initialize OneHotEncoder
+    onehotencoder = OneHotEncoder(drop='first')  # drop='first' to avoid dummy variable trap
+
+    # Apply one-hot encoder to categorical columns
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('cat', onehotencoder, categorical_cols)],
+        remainder='passthrough')  # keep other columns untouched
+
+    # Fit and transform the DataFrame
+    df_encoded = preprocessor.fit_transform(df)
+
+    # Feature names after one-hot encoding
+    feature_names = preprocessor.named_transformers_['cat'].get_feature_names(categorical_cols)
+
+    # Create a DataFrame with the new features
+    df_encoded = pd.DataFrame(df_encoded,
+                              columns=list(feature_names) + [col for col in df.columns if col not in categorical_cols])
+
+    return df_encoded
+
